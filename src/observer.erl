ae8a5cb88f1dfdfb7234dec4c5063010fdd05964
Dong Yuwei <newdongyuwei@gmail.com>
2016-12-10 22:36:27 +0800

    observer--worker link

---
+++ src/observer.erl
@@ -0,0 +1,39 @@
+-module (observer).
+-export ([start/0]).
+
+start() ->
+    spawn(fun() -> observer(fun() -> worker() end) end).
+    
+% c(observer).
+% observer:start().
+% worker_test ! test.
+% worker_test ! exit.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+observer(Fun) ->
+    io:format("observer started: ~p ~n", [self()]),
+    % the trap exit is needed, because if you don't have it the watching process will die if the linked process dies.
+    process_flag(trap_exit, true),
+    Pid = spawn_link(Fun),
+    register(worker_test, Pid), % `worker_test` for test in erl shell
+    receive
+        {'EXIT', Pid, Why} ->
+            io:format("worker died with:~p~n",[Why])
+    end.
+
+worker() ->
+    % register(worker_test, self()),
+    io:format("worker started: ~p ~n", [self()]),
+    worker_loop().
+
+worker_loop() ->
+    io:format("~p ~n", ["worker waiting for msg..."]),
+    receive 
+        exit ->
+            exit("oops, some one kill me.");
+        Any -> 
+            io:format("worker received msg: ~p ~n", [Any]),
+            worker_loop()
+    end.
